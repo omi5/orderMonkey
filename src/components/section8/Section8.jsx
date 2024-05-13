8f18a737cbfa3d8d7cc91b65644c79a8306c2ff0
omi5 <mamunomi24@gmail.com>
2024-05-13 11:22:36 +0600

    working on section 8 slider

--- src/components/section8/Section8.jsx
+++ src/components/section8/Section8.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Section8.css'
 import TopbarImage from '../../assets/images/whitebar1.png'
+import Section8Slider from '../section8Slider/Section8Slider'
+import Image from '../../assets/images/switzerlandSliderImage.png'
+import Logo from '../../assets/images/burgerMeister.png'
+
+const text = 'Taking dining to the next level with ORDERMONKEY, Burgermeister guests can experience faster service with dishes crafted to perfection and tailored to their unique tastes. Welcome to the future, where precision meets indulgence!'
 
 const Section8 = () => {
   return (
@@ -9,6 +14,9 @@ const Section8 = () => {
             <div className="section8-container">
                 <div className='section8-topbar-img'>
                     <img src={TopbarImage} alt="" />
+                </div>
+                <div className="slider-div">
+                    <Section8Slider image={Image} logo={Logo} title='Burgermeister Switzerland' text={text} />
                 </div>
             </div>
         </div>
---
+++ src/components/section8Slider/Section8Slider.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Section8Slider.css'
+
+const Section8Slider = ({image,title,text,logo}) => {
+  return (
+    <>
+        <div className="section8Slider-wrapper">
+            <div className="section8Slider-container">
+                <div className="section8Slider-img">
+                    <img src={image} alt="" />
+                </div>
+                <div className="section8SliderInfo">
+                    <h1>{title}</h1>
+                    <p>{text}</p>
+                    <div className='info-img'>
+                        <img src={logo} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Section8Slider
